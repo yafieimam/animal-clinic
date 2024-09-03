3d317bbe3f4389c5cb6e5c28815de233962557f8
Yafie Imam <yafie.achmad@intikom.co.id>
2024-09-03 11:05:54 +0700

    Update File

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -16,7 +16,7 @@
         <!-- BEGIN: JS Assets-->
         <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js">
         </script>
-        {{-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBG7gNHAhDzgYmq4-EHvM4bqW1DNj2UCuk&libraries=places">
+        {{-- <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
         </script> --}}
         <script src="{{ mix('dist/js/app.js') }}"></script>
         <!-- END: JS Assets-->
